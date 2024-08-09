9aaa297931b1b6348fac2b07e61318e159af3707
Ritesh Jadhav <riteshjadhav978@gmail.com>
2024-08-09 17:34:09 +0530

    changes in hero section

--- app/_components/CategoryList.js
+++ app/_components/CategoryList.js
@@ -5,7 +5,7 @@ function CategoryList({ categoryList }) {
   return (
     <div className="grid grid-cols-3 gap-7 lg:gap-4 md:grid-cols-3 lg:grid-cols-6 mt-10">
       {categoryList.map((category) => (
-        <div className={`flex flex-col items-center justify-center p-4 md:py-4 lg:py-4 gap-2 hover:scale-105 transition-transform duration-20 ease-linear bg-[#ffe0cc] rounded-md`} key={category.id}>
+        <div className={`flex flex-col items-center justify-center p-4 md:py-4 lg:py-4 gap-2 cursor-pointer hover:scale-105 transition-transform duration-20 ease-linear bg-[#ffe0cc] rounded-md`} key={category.id}>
           <Image
             src={category.icon.url}
             alt={`Icon for ${category.name}`}
