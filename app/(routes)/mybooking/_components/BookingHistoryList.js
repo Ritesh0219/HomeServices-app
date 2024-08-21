6872f12c6e9124451f8c631fa01a338ef2514a5d
Ritesh Jadhav <riteshjadhav978@gmail.com>
2024-08-22 00:36:13 +0530

    Minor fixes in Booking Section

--- app/(routes)/mybooking/_components/BookingHistoryList.js
+++ app/(routes)/mybooking/_components/BookingHistoryList.js
@@ -56,16 +56,9 @@ function BookingHistoryList({ bookingHistory, section }) {
                   <h2>
                     Booked :
                   </h2>
-                  <div className='flex flex-col md:flex-row md:gap-2 text-primary'>
-                    <h2>
-                     {formatDate(booking?.date) || 'Date'}
-                    </h2>
-                    <h2>
-                      {booking?.time}
-                    </h2>
-                  </div>
+                    {formatDate(booking?.date) || 'Date'}
                   </h2>
-                 
+                  
               </div>
             </div>
           ))}
