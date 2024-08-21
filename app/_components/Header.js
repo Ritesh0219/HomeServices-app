ceb26601fc40e56a7f69875422cd8524b1c8a22a
Ritesh Jadhav <riteshjadhav978@gmail.com>
2024-08-22 00:01:39 +0530

    Changes in Header Section

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -24,12 +24,13 @@ function Header() {
             </a>
             :
            <div className=' flex items-center gap-2 justify-between   text-primary  bg-white '>
+               
+               <Link href={'/mybooking'}>
+               <button className='bg-primary text-white px-3 py-2 text-[12px] text-center hover:bg-orange-700 rounded-full'>My Bookings</button>
+               </Link>
                <div className='border-[2px] flex items-center justify-center  border-primary rounded-full'>
                <UserButton/>
                </div>
-               <Link href={'/mybooking'}>
-               <button className='bg-primary text-white px-3 py-2  text-center hover:bg-orange-700 rounded-full'>My Bookings</button>
-               </Link>
            </div>
           }
             
