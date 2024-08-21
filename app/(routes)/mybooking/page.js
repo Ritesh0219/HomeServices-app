8819055ff15f37a16c1ad9738b5226e95856dc9f
Ritesh Jadhav <riteshjadhav978@gmail.com>
2024-08-21 23:59:06 +0530

    Changes in Booking Section

--- app/(routes)/mybooking/page.js
+++ app/(routes)/mybooking/page.js
@@ -41,26 +41,23 @@ function MyBooking() {
   return (
     <div className='w-full flex flex-col items-start mt-8 md:mt-5'>
       
-      <div className='flex flex-col gap-3 rounded-full px-8 py-1 mt-5'>
-        <div>
-        <h2 className=' md:mb-4 text-4xl font-extrabold'>My Bookings</h2>
-        </div>
-     <div className='flex gap-3 mt-4 md:mt-0 md:gap-4'>
-     <button
-          onClick={() => handleTabChange('Booked')}
-          className={`px-6 py-2 text-sm font-semibold rounded-full border ${activeTab === 'Booked' ? 'text-white bg-primary' : 'text-gray-500'}`}
-        >
-          Booked
-        </button>
-        <button
-          onClick={() => handleTabChange('Completed')}
-          className={`px-3 py-2 text-sm rounded-full font-semibold border ${activeTab === 'Completed' ? 'text-white bg-primary' : 'text-gray-500'}`}
-        >
-          Completed
-        </button>
-     </div>
+      <div className='flex flex-col items-center justify-center w-full'>
+      <h2 className=' md:mb-4 text-4xl font-extrabold'>Bookings</h2>
+          <div className=' flex gap-3 mt-5 md:mt-0'>
+            <button
+                onClick={() => handleTabChange('Booked')}
+                className={`px-6 py-2 text-sm font-semibold rounded-full border ${activeTab === 'Booked' ? 'text-white bg-primary' : 'text-gray-500'}`}
+              >
+                Booked
+              </button>
+              <button
+                onClick={() => handleTabChange('Completed')}
+                className={`px-3 py-2 text-sm rounded-full font-semibold border ${activeTab === 'Completed' ? 'text-white bg-primary' : 'text-gray-500'}`}
+              >
+                Completed
+              </button>
+          </div>
       </div>
-
       <div className='w-full  md:p-4'>
         {activeTab === 'Booked' && (
           <div className='w-full flex flex-col items-center justify-center'>
